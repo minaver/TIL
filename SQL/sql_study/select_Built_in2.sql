acd4551933330787b41923b02c64913c6e48057b
minaver <psmjoshua@gmail.com>
2021-09-21 23:17:39 +0900

    Add study built-in-calc-func

---
+++ SQL/sql_study/select_Built_in2.sql
@@ -0,0 +1,12 @@
+
+select truncate(112.456,1),truncate(112.456,2),truncate(112.456,-1),truncate(112.456,-2);
+
+select round(112.456,1),round(112.456,2),round(112.456,-1),round(112.456,-2);
+
+select ceil(10.2),ceil(0.7),ceil(-8.2),ceil(-1.9);
+
+select floor(10.2),floor(0.7),floor(-8.2),floor(-1.9);
+
+select mod(13,7),mod(33,4),mod(4,1);
+
+select power(2,3),power(3,4),power(4,0),power(5,-2);

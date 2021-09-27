61941eb8f4916b619f7b912d3ca32cdcb8f9839a
minaver <psmjoshua@gmail.com>
2021-09-27 23:07:38 +0900

    modify Hw3(add discription remark)

--- DataScience/Hw3/DataSummary_hw(1,2,3,4)_201713056.py
+++ DataScience/Hw3/DataSummary_hw(1,2,3,4)_201713056.py
@@ -6,6 +6,7 @@ xl_file = 'db_score.xlsx'
 df = pd.read_excel(xl_file)
 
 # (1) mean, median (midterm, final, score 각각)
+# midterm, final, score 값을 각각 받아와 mean 함수로 mean 출력 median 함수로 meadian 출력
 mid_mean = np.mean(df.midterm)
 fin_mean = np.mean(df.final)
 scr_mean = np.mean(df.score)
@@ -26,12 +27,14 @@ print(f"final : {fin_median}")
 print(f"score : {scr_median}")
 
 # (2) mode (grade)
+# from scipy.stats import mode 를 통해 mode를 import 하고 값만 출력하기 위해 [0] 추가한다.
 grade_mode = mode(df.grade)[0]
 
 print("\n(2) mode (grade)")
 print(f"grade : {grade_mode}")
 
 # (3) variance, standard deviation (midterm, final, score 각각)
+# midterm, final, score 값을 각각 받아와 var 함수로 variance, std 함수로 standard deviation를 구한다.
 mid_var = np.var(df.midterm)
 fin_var = np.var(df.final)
 scr_var = np.var(df.score)
@@ -52,6 +55,7 @@ print(f"final : {fin_std}")
 print(f"score : {scr_std}")
 
 # (4) percentile plot (midterm, final, score 각각)
+# midterm, final, score 값을 각각 받아와 for loop을 통해 0부터 100까지 10 단위로의 백분위수를 구한다.
 print("\n(4) percentile plot (midterm, final, score 각각)")
 for p in range(0,110,10):
     print(f"midterm {p}-th percentile : {np.percentile(df.midterm,p)}")
--- DataScience/Hw3/DataSummary_hw(5,6,7)_201713056.py
+++ DataScience/Hw3/DataSummary_hw(5,6,7)_201713056.py
@@ -7,6 +7,7 @@ xl_file = 'db_score.xlsx'
 df = pd.read_excel(xl_file)
 
 # (5) boxplot (midterm, final, score 각각)
+#  midterm, final, score 값을 column값으로 boxplot 함수에 대입한다. 교재 예시 boxplot과 유사하게 grid를 제거해주었다.(grid=False)
 print("\n(5) boxplot (midterm, final, score 각각)")
 boxplot = df.boxplot(column=['midterm', 'final', 'score'],grid=False)
 
@@ -14,6 +15,7 @@ boxplot.plot()
 plt.show()
 
 # (6) histogram (midterm, final, score 각각)
+# midterm, final, score값을 받아와 hist()함수를 통해 histogram을 그린다. 각각 x축에 이름을 부여하였다.(plt.xlabel()).
 print("\n(6) histogram (midterm, final, score 각각)")
 mid_hist = df.midterm.plot.hist()
 plt.xlabel('midterm')
@@ -26,6 +28,7 @@ plt.xlabel('score')
 plt.show()
 
 # (7) scatter plot (midterm, final, score 에 대한, 모든 가능한 attribute 조합에 대하여 그릴 것)
+# 가능한 조합은 midterm과 final, final과 score, midterm과 score 3가지이다. 각각의 조합을 x,y에 배치해 scatter plot을 그린다.
 print("\n(7) scatter plot (midterm, final, score 에 대한, 모든 가능한 attribute 조합에 대하여 그릴 것)")
 plt.scatter(df.midterm,df.final)
 plt.xlabel('midterm')

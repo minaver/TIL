2c9ab818e66151834948d5243c0b2928d687f5ea
minaver <psmjoshua@gmail.com>
2021-10-10 23:14:43 +0900

    modify final score classification multiclass

--- DataScience/Hw4/score_Classification_multiclass.py
+++ DataScience/Hw4/score_Classification_multiclass.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[64]:
-
-
 import pymysql
 import numpy as np
 
@@ -29,9 +26,15 @@ def classification_performance_eval(y,y_predict,target):
     
     accuracy = (tp+tn) / (tp+tn+fp+fn)
     precision = tp / (tp+fp)
-    recall = tp / (tp+fn)
-    f1_score = (2*precision*recall) / (precision+recall)
-    
+    try:
+        recall = tp / (tp+fn)
+    except:
+        recall = 0
+    try:
+        f1_score = (2*precision*recall) / (precision+recall)
+    except:
+        f1_score = 0
+        
     return accuracy , precision, recall, f1_score
 # /5-1
 
@@ -106,9 +109,46 @@ print("precision : ",pre)
 print("recall : ",rec)
 print("f1-score : ",f1)
 
+# [K-fold 사용]
+print()
+from sklearn.model_selection import KFold
+from sklearn import tree
 
-# In[ ]:
+acc = []
+pre = []
+rec = []
+f1 = []
 
+# 2. K-fold attribute 설정
+kf = KFold(n_splits=5,random_state=42,shuffle=True)
 
+# 3. K-fold에서 추출한 train과 test index를 기준으로 X,y 각각의 train,test data를 설정해준다.
+# kf.split(X)는 n_splits만큼의 경우를 갖고있다.
+import statistics
+for target in ['A','B','C']:
+    for train_index,test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
+        # 3-1. SVM를 사용하여 학습 진행 (using X_train,y_train)
+        svc_model = svm.SVC(kernel = 'linear', C=100) #kernel = 'rbf', gamma = 0.4, C=1
+        svc_model.fit(X_train,y_train)
 
+        # 4-1. SVC model predict(predict 함수에 X_test data를 사용하여 y_predict를 도출)
+        y_predict = svc_model.predict(X_test)
+        
+        # 5. 도출한 y_predict와 y_test를 비교하여 성능 평가
+        accuracy,precision,recall,f1_score = classification_performance_eval(y_test,y_predict,target)
+
+        # 6. 각 성능 배열에 append하여 추가
+        acc.append(accuracy)
+        pre.append(precision)
+        rec.append(recall)
+        f1.append(f1_score)
+
+    # 7. 각 성능의 평균값 구한다.
+    print("[\"Grade",target,"\" SVC & K-fold 성능 평가]")
+    print("accuracy :",statistics.mean(acc))
+    print("precision :",statistics.mean(pre))
+    print("recall : ",statistics.mean(rec))
+    print("f1-score : ",statistics.mean(f1))
